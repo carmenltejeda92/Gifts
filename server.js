341a612191f426032159ec38d8c434d3eccb8222
carmenltejeda92 <carmenltejeda92@gmail.com>
2024-01-03 03:25:56 -0500

    Routes are working

--- server.js
+++ server.js
@@ -11,18 +11,22 @@ app.engine("jsx", require("express-react-views").createEngine())
 app.use(express.urlencoded({extended:false}))
 
 //-------------------[Routes]
-app.get("/", (req, res)=>{
-    res.send("<h1>Hello Bish</h1>")
-})
+// app.get("/", (req, res)=>{
+//     res.send("<h1>Hello Bish</h1>")
+// })
 
-app.get("/home", (req, res)=>{
+app.get("/", (req, res)=>{
     res.render("Home")
 })
 
 
-
+// if statement to display if something's at that index of the array
 app.get('/:indexOfGiftArray', (req, res)=>{
-    res.send(giftsss[req.params.indexOfGiftArray])
+    if(giftsss[req.params.indexOfGiftArray]){
+        res.send(giftsss[req.params.indexOfGiftArray])
+    }else{
+        res.send('There are no more items in Santas bag here ' + req.params.indexOfGiftArray)
+    }
 })
 
 //--------------------[Server]
--- views/Home.jsx
+++ views/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Home.css';
+
 
 function Home() {
+
+
+  
   return (
     <div>
-      This ish worked
+      <div><h1>Welcome to Santa's Workshop</h1></div>
+      <div><h3>Come inside!</h3></div>
     </div>
   )
 }
